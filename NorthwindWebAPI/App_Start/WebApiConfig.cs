55c263a122dbd819c7c8cd8f7add73ddf8d75d0b
sean <armstrong.d.sean@gmail.com>
2018-09-03 11:21:07 -0400

    first commit and made adjustments for infinite looping

---
+++ NorthwindWebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace NorthwindWebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
