9ace8c9533ace749049902288dbb1b118c054fab
sean <armstrong.d.sean@gmail.com>
2018-09-03 13:14:55 -0400

    have methods set up to handle url parameters to return info that is requested

--- NorthwindWebAPI/Controllers/ProductsController.cs
+++ NorthwindWebAPI/Controllers/ProductsController.cs
@@ -17,23 +17,59 @@ namespace NorthwindWebAPI.Controllers
         private NorthwindWebAPIModel db = new NorthwindWebAPIModel();
 
         // GET: api/Products
-        public IQueryable<Product> GetProducts()
+        public IQueryable<ProductViewModel> GetProducts()
         {
-            return db.Products;
+            return db.Products.ToList().Select(p => new ProductViewModel
+            {
+                ProductID = p.ProductID,
+                ProductName = p.ProductName
+            }).AsQueryable();
+        }
+
+        public IQueryable<Product> GetProducts(string id)
+        {
+            var productId = Convert.ToInt32(Request.GetQueryNameValuePairs().Where(nv => nv.Key == "id").Select(nv => nv.Value)
+                .FirstOrDefault());
+            var productById = db.Products.Where(m => m.ProductID == productId);
+            return productById;
+        }
+
+        public IQueryable<Product> GetProductsByCategoryId(string categoryid)
+        {
+            var categoryId = Convert.ToInt32(Request.GetQueryNameValuePairs().Where(nv => nv.Key == "categoryid").Select(nv => nv.Value)
+                .FirstOrDefault());
+            var productByCategoryId = db.Products.Where(m => m.CategoryID == categoryId);
+            return productByCategoryId;
+        }
+
+        public IQueryable<Product> GetProductsBySupplierId(string supplierid)
+        {
+            var supplierId = Convert.ToInt32(Request.GetQueryNameValuePairs().Where(nv => nv.Key == "supplierid").Select(nv => nv.Value)
+                .FirstOrDefault());
+            var productBySupplierId = db.Products.Where(m => m.SupplierID == supplierId);
+            return productBySupplierId;
+        }
+
+        public IQueryable<Product> GetProductsByMaxPrice(string maxprice)
+        {
+            var maxPrice = Convert.ToDecimal(Request.GetQueryNameValuePairs().Where(nv => nv.Key == "maxprice").Select(nv => nv.Value)
+                .FirstOrDefault());
+            var productByMaxPrice = db.Products.Where(m => m.UnitPrice <= maxPrice);
+            return productByMaxPrice;
         }
 
         // GET: api/Products/5
-        [ResponseType(typeof(Product))]
-        public IHttpActionResult GetProduct(int id)
-        {
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
+        //[ResponseType(typeof(Product))]
+        //public IHttpActionResult GetProduct(int id)
+        //{
+        //    Product product = db.Products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(product);
-        }
+        //    return Ok(product);
+        //}
 
         // PUT: api/Products/5
         [ResponseType(typeof(void))]
